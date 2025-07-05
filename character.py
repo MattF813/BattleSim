0733df0ed7dc60ed32a93a997f0c989362a1eb44
ShaneRung8820 <Shane.rung@gmail.com>
2025-07-05 16:30:06 -0400

    Turn Update

--- character.py
+++ character.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Character:
     def __init__(self, name, health, attack):
         self.name = name
@@ -24,13 +26,33 @@ class Hero(Character):
         self.magic = magic
         self.lvl = 1
 
-    def attackEnemy(self, enemy):
+    def attackEnemy(self, enemyList: List[Character]):
+        enemyNum = len(enemyList)
+        enemyChoice = enemyNum
+        while (True):
+            if (enemyNum == 1):
+                enemyChoice = 0
+                break
+            enemyChoice = input("There are " + str(enemyNum) + " enemies. Please select your target by entering a number from 1-" + str(enemyNum) + ": ")
+            enemyChoice = int(enemyChoice)
+            if (enemyChoice < 0 or enemyChoice > len(enemyList)):
+                print("Number chosen is not a valid target. Please try again.")
+            else:
+                enemyChoice -= 1
+                break
+        enemy = enemyList[enemyChoice]
         super().attackEnemy(enemy)
         if enemy.killed:
             self.magic = self.magicMax
             self.health = self.healthMax
             self.lvl += 1
             print("Congratulations! " + self.name + " has reached level " + str(self.lvl) + ". Health and Mana have been restored!")
+            return [1, enemy]
+        return [0, enemy]
+    def takeTurn(self, enemyList: List[Character]):
+        choice = input("Please select from the following options: \n1. Attack \n")
+        if choice == "1":
+            return self.attackEnemy(enemyList)
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from character import Character, Hero
+from typing import List
+
+def startEncounter(hero: Character, enemyList: List[Character]):
+    while (not hero.killed and len(enemyList) > 0):
+        killed = hero.takeTurn(enemyList)
+        if killed[0] == 1:
+            enemy = killed[1]
+            enemyList.remove(enemy)
+        for enemy in enemyList:
+            enemy.attackEnemy(hero)
 
 if __name__ == '__main__':
     charName = input("Welcome to Battle Simulator! Please enter your name: ")
@@ -12,12 +22,5 @@ if __name__ == '__main__':
             print("Class not found. Please try again")
     print("" + mainGuy.name + " wakes up in a dark cave. A fearsome goblin stands in front of them, ready for battle!")
     enemy1 = Character("Goblin", 10, 5)
-    while (not mainGuy.killed and not enemy1.killed):
-        print("" + mainGuy.name + "'s turn! Select one of the following options:")
-        choice = input("1. Attack \n")
-        if choice == str(1):
-            mainGuy.attackEnemy(enemy1)
-            if (enemy1.killed):
-                break
-        print("Enemy turn!")
-        enemy1.attackEnemy(mainGuy)
+    enemy2 = Character("Goblin2", 5, 8)
+    startEncounter(mainGuy, [enemy1, enemy2])
